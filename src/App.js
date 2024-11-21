4074f5c9a72b14773b12ae15c2759dea46a227fd
Shinji0133 <adrwss2@gmail.com>
2024-11-21 19:00:50 +0900

    feat: implement basic to-do list to do list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import Button from './Button';
-import styles from './App.module.css';
+import { useState, useEffect } from 'react';
+import React from 'react';
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const onChange = event => setTodo(event.target.value);
+  const onSubmit = event => {
+    event.preventDefault();
+    if (todo === '') {
+      return;
+    }
+
+    setTodos(curr => [todo, ...curr]);
+    setTodo('');
+  };
+  console.log(todos);
+
   return (
     <div>
-      <h1 className={styles.h1}>Welcome back!!</h1>
-      <Button text="Click me" />
+      <h1>My To Do List ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={todo}
+          type="text"
+          onChange={onChange}
+          placeholder="hello"
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
